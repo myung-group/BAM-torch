57a1e38edf3d907896b8890eb4e810ff9fc2d66d
SWKim <peillord@yahoo.co.jp>
2026-03-26 10:49:15 +0900

    Integrate charge-dependent models into BAM-torch registry
    - Register ChargeRACE/ChargeRACEv3 in model registry
    - Register CDTrainer/CDTrainerV3 in trainer registry
    - Add ase dependency to pyproject.toml
    - Update .gitignore for charge-dependent artifacts

--- bam_torch/training/__init__.py
+++ bam_torch/training/__init__.py
@@ -2,7 +2,12 @@ from .base_trainer import BaseTrainer
 from .mp_trainer import MPTrainer, MPTrainer_V2
 from .mve_trainer import MVETrainer
 from .multihead_trainer import MultiheadTrainer
-from bam_torch.group_averaging.training.ga_trainer import GATrainer
+
+try:
+    from bam_torch.group_averaging.training.ga_trainer import GATrainer
+    _GA_AVAILABLE = True
+except (ImportError, OSError):
+    _GA_AVAILABLE = False
 
 
 TRAINER_REGISTRY = {
@@ -14,8 +19,25 @@ TRAINER_REGISTRY = {
     "materials_project": MPTrainer_V2,
     "mp_v1": MPTrainer,
     "mp_v2": MPTrainer_V2,
-    "ga": GATrainer,
-    "group_averaging": GATrainer,
-    "frame_averaging": GATrainer,
-    "probabilistic_symmetrization": GATrainer,
 }
+
+if _GA_AVAILABLE:
+    TRAINER_REGISTRY["ga"] = GATrainer
+    TRAINER_REGISTRY["group_averaging"] = GATrainer
+    TRAINER_REGISTRY["frame_averaging"] = GATrainer
+    TRAINER_REGISTRY["probabilistic_symmetrization"] = GATrainer
+
+try:
+    from bam_torch.charge_dependent.training.cd_trainer import CDTrainer
+    TRAINER_REGISTRY["cd"] = CDTrainer
+    TRAINER_REGISTRY["charge_dependent"] = CDTrainer
+except ImportError:
+    pass
+
+try:
+    from bam_torch.charge_dependent.training.cd_trainer_v3 import CDTrainerV3
+    TRAINER_REGISTRY["cd_v3"] = CDTrainerV3
+    TRAINER_REGISTRY["charge_dependent_v3"] = CDTrainerV3
+    TRAINER_REGISTRY["cd_e"] = CDTrainerV3
+except ImportError:
+    pass
